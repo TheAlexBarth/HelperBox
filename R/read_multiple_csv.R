bbe4c65b02f668ebd08fe478948838bfa18d3328
Alex Barth <abarth1014@gmail.com>
2021-11-04 17:42:50 -0400

    Created read_multiple_csv function

---
+++ R/read_multiple_csv.R
@@ -0,0 +1,24 @@
+#' read_multiple_csv - read multiple csv files
+#'
+#' This function will take multiple csv files
+#' It will read them and return all as one dataframe
+#' It is it will read files in the order of provided index
+#'
+#' @param index the index of all csv files in the working directory
+#' @param header to talk to read.csv, default true
+#' @param as.is to talk to read.csv, default true
+#'
+#' @importFrom utils read.csv
+#'
+#' @export
+read_multiple_csv <- function(index, header = TRUE, as.is = TRUE){
+
+  #initialize holder list
+  list_file <- vector(mode = "list", length = length(index))
+
+  for(i in 1:length(index)){
+    list_file[[i]] <- read.csv(dir()[index[i]], header = header, as.is = as.is)
+  }
+  output_df <- do.call(rbind,list_file)
+
+}
