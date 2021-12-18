9f46f9b4e8e2112b9cdc1afb33b0113b7ad9c1d3
Alex Barth <AB93@email.sc.edu>
2021-12-18 09:12:45 -0500

    added path arg

--- R/read_multiple_csv.R
+++ R/read_multiple_csv.R
@@ -4,6 +4,7 @@
 #' It will read them and return all as one dataframe
 #' It is it will read files in the order of provided index
 #'
+#' @param path the folder you want to pull from
 #' @param index the index of all csv files in the working directory
 #' @param header to talk to read.csv, default true
 #' @param as.is to talk to read.csv, default true
@@ -17,7 +18,7 @@ read_multiple_csv <- function(index, header = TRUE, as.is = TRUE){
   list_file <- vector(mode = "list", length = length(index))
 
   for(i in 1:length(index)){
-    list_file[[i]] <- read.csv(dir()[index[i]], header = header, as.is = as.is)
+    list_file[[i]] <- read.csv(dir(path)[index[i]], header = header, as.is = as.is)
   }
   output_df <- do.call(rbind,list_file)
 
